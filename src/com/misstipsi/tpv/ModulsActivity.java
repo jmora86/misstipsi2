2f00aa549bd8e8decb0d709242dbe0ac7749b9cb
jmora86 <jmora@misstipsi.com>
2014-01-19 22:03:25 +0100

    Add ModulsActivity - First

---
+++ src/com/misstipsi/tpv/ModulsActivity.java
@@ -0,0 +1,17 @@
+package com.misstipsi.tpv;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ModulsActivity extends Activity 
+{
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.moduls);
+    }
+ 
+   
+}
